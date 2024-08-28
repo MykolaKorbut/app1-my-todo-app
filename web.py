d065d23291df708f9504d3c4991275b2b4de5cb7
mikolakorbut <nikolay.korbut@gmail.com>
2024-08-28 12:07:35 +0300

    add html features

--- web.py
+++ web.py
@@ -12,7 +12,8 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase <b>your</b> productivity.",
+         unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
